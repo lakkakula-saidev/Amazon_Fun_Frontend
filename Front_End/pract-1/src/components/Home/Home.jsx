7894cbecf5ad63e75d183ede5e01996d70a02b9f
Saidev Lakkakula <lakkakula.saidev@gmail.com>
2021-06-04 16:33:59 +0200

    Updated Brands

--- Front_End/pract-1/src/components/Home/Home.jsx
+++ Front_End/pract-1/src/components/Home/Home.jsx
@@ -11,7 +11,12 @@ class Home extends Component {
         filteredProducts: null,
         isLoading: true,
         price: 10,
-        brand: []
+        brand: [],
+        averagePrice: null,
+        priceRange: [],
+        minPrice: null,
+        maxPrice: null,
+        allBrands: []
     };
 
     async componentDidMount() {
@@ -19,10 +24,34 @@ class Home extends Component {
         /* this.setState({ products: response.result, isLoading: false }); */
         this.setState({ allProducts: response.result, isLoading: false });
         console.log(response.result);
+        const brands = new Set(this.state.allProducts.map((item) => item.brand));
+        this.setState({ allBrands: [...this.state.allBrands, brands] });
+        console.log(brands);
+        const Prices = this.state.allProducts.map((item) => item.price);
+        console.log(this.state.allBrands);
     }
+
+    /**
+     * length / 3
+     *
+     */
 
     handleCheckbox(e) {
         console.log(e.target.key);
+        if (e.target.checked) {
+            this.setState({ brand: [...this.state.brand, e.target.id] });
+            console.log(this.state.brand);
+        } else {
+            const brand = [...this.state.brand]; // make a separate copy of the array
+            const index = brand.indexOf(e.target.id);
+            if (index !== -1) {
+                brand.splice(index, 1);
+                this.setState({ brand: brand });
+            }
+        }
+    }
+
+    handlePrice(e) {
         if (e.target.checked) {
             this.setState({ brand: [...this.state.brand, e.target.id] });
             console.log(this.state.brand);
@@ -48,8 +77,10 @@ class Home extends Component {
     }
     render() {
         console.log(this.state.allProducts);
+        /*     const brands = this.state.allBrands; */
         const allProducts = this.state.allProducts;
         const filteredProducts = this.state.filteredProducts;
+
         return (
             <Container fluid>
                 <h1>Our Products</h1>
@@ -62,29 +93,35 @@ class Home extends Component {
                                 <Row className="checkBox1">
                                     <h6>Brand</h6>
                                     <Form>
-                                        {allProducts.map((item) => (
-                                            <Row>
-                                                <Form.Check
-                                                    className="d-flex align-items-start"
-                                                    onChange={(e) => this.handleCheckbox(e)}
-                                                    label={item.brand}
-                                                    name="group1"
-                                                    type="checkbox"
-                                                    key={item._id}
-                                                    id={item.brand}
-                                                />
-                                            </Row>
-                                        ))}
+                                        {allProducts.length > 0 ? (
+                                            allProducts.map((item) => (
+                                                <Row>
+                                                    <Form.Check
+                                                        className="d-flex align-items-start"
+                                                        onChange={(e) => this.handleCheckbox(e)}
+                                                        label={item.brand}
+                                                        name="group1"
+                                                        type="checkbox"
+                                                        key={item._id}
+                                                        id={item.brand}
+                                                    />
+                                                </Row>
+                                            ))
+                                        ) : (
+                                            <></>
+                                        )}
                                     </Form>
                                 </Row>
                                 <Row className="checkBox1">
                                     <h6>Price</h6>
                                     <Form>
-                                        {allProducts.map((item) => (
-                                            <Row>
-                                                <Form.Check className="d-flex align-items-start " label={item.price} name="group1" type="checkbox" key={item._id} id={item._id} />
-                                            </Row>
-                                        ))}
+                                        <Row>
+                                            <Form.Check className="d-flex align-items-start " onChange={(e) => this.handlePrice(e)} label="< 10" name="group1" type="checkbox" id={10} />
+                                            <Form.Check className="d-flex align-items-start " onChange={(e) => this.handlePrice(e)} label="10-20" name="group1" type="checkbox" id={20} />
+                                            <Form.Check className="d-flex align-items-start " onChange={(e) => this.handlePrice(e)} label="20-50" name="group1" type="checkbox" id={50} />
+                                            <Form.Check className="d-flex align-items-start " onChange={(e) => this.handlePrice(e)} label="less than 100" name="group1" type="checkbox" id={100} />
+                                            <Form.Check className="d-flex align-items-start " onChange={(e) => this.handlePrice(e)} label="above 100" name="group1" type="checkbox" id={100} />
+                                        </Row>
                                     </Form>
                                 </Row>
                             </Col>
--- Front_End/pract-1/src/components/Product/AddProduct/AddProduct.jsx
+++ Front_End/pract-1/src/components/Product/AddProduct/AddProduct.jsx
@@ -1,228 +1,199 @@
-import React, {Component} from "react";
-import {Container, Col, Form, Row, Button} from "react-bootstrap";
-
+import React, { Component } from "react";
+import { Container, Col, Form, Row, Button } from "react-bootstrap";
+import Post from "../../CRUD/PostProduct.js";
 const apiUrl = process.env.REACT_APP_BE_URL;
 
 class AddProduct extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			product: {
-				productName: "",
-				brand: "",
-				model: "",
-				description: "",
-				image: "",
-			},
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            product: {
+                name: "",
+                brand: "",
+                price: "",
+                description: ""
+                /* image: "" */
+            }
+        };
+    }
 
-	GetProduct = async () => {
-		try {
-			const request = await fetch(
-				apiUrl + `/products/${this.props.match.params.id}`
-			);
-			const response = await request.json();
-			this.setState({
-				product: response,
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    GetProduct = async () => {
+        try {
+            const request = await fetch(apiUrl + `/products/${this.props.match.params.id}`);
+            const response = await request.json();
+            this.setState({
+                product: response
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-	AddProduct = async () => {
-		try {
-			await fetch(apiUrl + `/products/`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(this.state.product),
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    AddProduct = async () => {
+        try {
+            await fetch(apiUrl + `/products/`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(this.state.product)
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-	UpdateProduct = async () => {
-		try {
-			await fetch(apiUrl + `/products/${this.props.match.params.id}`, {
-				method: "PUT",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(this.state.product),
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    UpdateProduct = async () => {
+        try {
+            await fetch(apiUrl + `/products/${this.props.match.params.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(this.state.product)
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-	AddFile = async () => {
-		try {
-			const request = await fetch(apiUrl + `/products/upload`, {
-				method: "POST",
-				body: this.state.image,
-			});
-			if (request.ok) {
-				const response = await request.json();
-				this.setState({
-					product: {
-						...this.state.product,
-						image: response.file,
-					},
-				});
-				if (this.props.match.params.id) {
-					this.UpdateProduct();
-				} else {
-					this.AddProduct();
-				}
-				console.log(response.file);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    AddFile = async () => {
+        try {
+            const request = await fetch(apiUrl + `/products/upload`, {
+                method: "POST",
+                body: this.state.image
+            });
+            if (request.ok) {
+                const response = await request.json();
+                this.setState({
+                    product: {
+                        ...this.state.product,
+                        image: response.file
+                    }
+                });
+                if (this.props.match.params.id) {
+                    this.UpdateProduct();
+                } else {
+                    this.AddProduct();
+                }
+                console.log(response.file);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-	HandleProduct = (event) => {
-		event.preventDefault();
+    HandleProduct = (event) => {
+        event.preventDefault();
 
-		const file = document.getElementById("image").files.length;
-		if (file > 0) {
-			this.AddFile();
-		} else if (this.props.match.params.id) {
-			this.UpdateProduct();
-			this.setState({
-				product: {},
-			});
-		} else {
-			this.AddProduct();
-			this.setState({
-				product: {},
-			});
-		}
-	};
+        const file = document.getElementById("image").files.length;
+        if (file > 0) {
+            this.AddFile();
+        } else if (this.props.match.params.id) {
+            this.UpdateProduct();
+            this.setState({
+                product: {}
+            });
+        } else {
+            this.AddProduct();
+            this.setState({
+                product: {}
+            });
+        }
+    };
 
-	HandleSetting = () => {
-		if (this.props.match.params.id) {
-			this.GetProduct();
-		}
-	};
-	componentDidMount() {
-		this.HandleSetting();
-	}
+    HandleSetting = () => {
+        if (this.props.match.params.id) {
+            this.GetProduct();
+        }
+    };
+    componentDidMount() {
+        this.HandleSetting();
+    }
 
-	handleInputChange = (event) => {
-		const id = event.target.id;
-		const value = event.target.value;
+    handleInputChange = (event) => {
+        const id = event.target.id;
+        const value = event.target.value;
 
-		this.setState({
-			product: {
-				...this.state.product,
-				[id]: value,
-			},
-		});
-	};
+        this.setState({
+            product: {
+                ...this.state.product,
+                [id]: value
+            }
+        });
+    };
 
-	handleInputImage = (event) => {
-		event.preventDefault();
-		const id = event.target.id;
-		const file = event.target.files[0];
-		let form = new FormData();
-		form.append("img", file);
+    handleInputImage = (event) => {
+        event.preventDefault();
+        const id = event.target.id;
+        const file = event.target.files[0];
+        let form = new FormData();
+        form.append("img", file);
 
-		this.setState({
-			product: {
-				...this.state.product,
-			},
-			[id]: form,
-		});
-	};
+        this.setState({
+            product: {
+                ...this.state.product
+            },
+            [id]: form
+        });
+    };
 
-	render() {
-		return (
-			<Container>
-				<Row className="mt-3 mb-5">
-					<Col sm={10} md={7} className="m-auto border border-success p-5">
-						<Form onSubmit={this.HandleProduct}>
-							<Form.Group
-								controlId="productName"
-								className="d-flex justify-content-between mb-2"
-							>
-								<Form.Label>Product Name</Form.Label>
-								<Form.Control
-									value={this.state.product.productName}
-									onChange={this.handleInputChange}
-									className="w-50"
-									type="text"
-									placeholder="Enter product name"
-								/>
-							</Form.Group>
+    async postData(e) {
+        e.preventDefault();
+        let Value = !Object.values(this.state.product).some((element) => element === null);
+        if (Value) {
+            let response = await Post("products", this.state.product);
+            console.log("Data is posted, Cover is yet to be....");
+            /* if (this.state.blogPostCover !== null) {
+                response = await response.json();
+                let formData = new FormData();
+                formData.append("uploadCover", this.state.blogPostCover);
+                let _id = response._id;
+                const post_truthy = await FormDataPost(formData, _id, "blogPosts", "uploadCover");
+            } */
+        }
+    }
 
-							<Form.Group
-								controlId="brand"
-								className="d-flex justify-content-between mb-2"
-							>
-								<Form.Label>Product brand</Form.Label>
-								<Form.Control
-									value={this.state.product.brand}
-									onChange={this.handleInputChange}
-									className="w-50"
-									type="text"
-									placeholder="Enter product brand"
-								/>
-							</Form.Group>
+    render() {
+        console.log(this.state.product);
+        return (
+            <Container>
+                <Row className="mt-3 mb-5">
+                    <Col sm={10} md={7} className="m-auto border border-success p-5">
+                        <Form onSubmit={(e) => this.postData(e)}>
+                            <Form.Group controlId="name" className="d-flex justify-content-between mb-2">
+                                <Form.Label>Product Name</Form.Label>
+                                <Form.Control value={this.state.product.productName} onChange={this.handleInputChange} className="w-50" type="text" placeholder="Enter product name" />
+                            </Form.Group>
 
-							<Form.Group
-								controlId="model"
-								className="d-flex justify-content-between mb-2"
-							>
-								<Form.Label>Product Model</Form.Label>
-								<Form.Control
-									value={this.state.product.model}
-									onChange={this.handleInputChange}
-									className="w-50"
-									type="text"
-									placeholder="Enter product model"
-								/>
-							</Form.Group>
+                            <Form.Group controlId="brand" className="d-flex justify-content-between mb-2">
+                                <Form.Label>Product brand</Form.Label>
+                                <Form.Control value={this.state.product.brand} onChange={this.handleInputChange} className="w-50" type="text" placeholder="Enter product brand" />
+                            </Form.Group>
 
-							<Form.Group
-								className="d-flex justify-content-between mb-2"
-								controlId="description"
-							>
-								<Form.Label>Product Description</Form.Label>
-								<Form.Control
-									value={this.state.product.description}
-									onChange={this.handleInputChange}
-									className="w-50"
-									type="text"
-									placeholder="Enter product description"
-								/>
-							</Form.Group>
+                            <Form.Group controlId="price" className="d-flex justify-content-between mb-2">
+                                <Form.Label>Product Price</Form.Label>
+                                <Form.Control value={this.state.product.price} type="number" onChange={this.handleInputChange} className="w-50" placeholder="Enter product price" />
+                            </Form.Group>
 
-							<Form.Group
-								className="d-flex justify-content-between mb-2"
-								controlId="image"
-							>
-								<Form.Label>Image</Form.Label>
-								<Form.Control
-									onChange={this.handleInputImage}
-									className="w-75"
-									type="file"
-									placeholder="Enter product image url"
-								/>
-							</Form.Group>
-							<Button className="m-2" variant="primary" type="submit">
-								{this.props.match.params.id ? "Update Product" : "Add Product"}
-							</Button>
-						</Form>
-					</Col>
-				</Row>
-			</Container>
-		);
-	}
+                            <Form.Group className="d-flex justify-content-between mb-2" controlId="description">
+                                <Form.Label>Product Description</Form.Label>
+                                <Form.Control value={this.state.product.description} onChange={this.handleInputChange} className="w-50" type="text" placeholder="Enter product description" />
+                            </Form.Group>
+
+                            <Form.Group className="d-flex justify-content-between mb-2" controlId="image">
+                                <Form.Label>Image</Form.Label>
+                                <Form.Control onChange={this.handleInputImage} className="w-75" type="file" placeholder="Enter product image url" />
+                            </Form.Group>
+                            <Button className="m-2" variant="primary" type="submit">
+                                {this.props.match.params.id ? "Update Product" : "Add Product"}
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default AddProduct;
